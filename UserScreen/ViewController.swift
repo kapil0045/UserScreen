26aab03a48fc0d598ba74a502d2318525305c09e
Ruchika <kapil.ecsion@gmail.com>
2021-12-22 12:52:02 +0530

    Initial Commit

---
+++ UserScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserScreen
+//
+//  Created by phonestop on 12/22/21.
+//  Copyright © 2021 phonestop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
